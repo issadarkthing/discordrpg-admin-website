568bc3addea251b8dad762ab08ed1c64c37ff2c2
raziman <raziman@tutanota.com>
2022-03-16 20:12:31 +0800

    move providers to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,41 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { AlertProvider } from "../components/AlertProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export const themeOptions = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: '#15465b',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      paper: '#091119',
+      default: '#082235',
+    },
+    text: {
+      primary: "#fff",
+    },
+    divider: "rgba(255, 255, 255, 0.12)",
+  },
+});
+
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={themeOptions}>
+      <AlertProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </AlertProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,8 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import Dashboard from "../components/Dashboard";
-import { AlertProvider } from "../components/AlertProvider";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export const themeOptions = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: '#15465b',
-    },
-    secondary: {
-      main: '#f50057',
-    },
-    background: {
-      paper: '#091119',
-      default: '#082235',
-    },
-    text: {
-      primary: "#fff",
-    },
-    divider: "rgba(255, 255, 255, 0.12)",
-  },
-});
-
-export const queryClient = new QueryClient();
 
 function App() { 
-  
-  return (
-    <div>
-      <ThemeProvider theme={themeOptions}>
-        <AlertProvider>
-          <QueryClientProvider client={queryClient}>
-            <Dashboard />
-            {/* <Login /> */}
-          </QueryClientProvider>
-        </AlertProvider>
-      </ThemeProvider>
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
