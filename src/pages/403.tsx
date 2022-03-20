aec31e6f8308694639e3bb85b87e0268e277c439
raziman <raziman@tutanota.com>
2022-03-20 16:23:47 +0800

    add unauthorized page

---
+++ src/pages/403.tsx
@@ -0,0 +1,28 @@
+import { CssBaseline, Grid, Typography, Link } from "@mui/material";
+
+
+
+export default function() {
+
+  return (
+    <div>
+      <CssBaseline />
+      <Grid 
+        container 
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ height: "100vh" }}
+      >
+        <Grid item>
+          <Typography variant="h4">
+            403 Unauthorized ⚠️
+          </Typography>
+          <Typography variant="body1">
+            Go back to <Link href="/" color="secondary">main</Link> page
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
